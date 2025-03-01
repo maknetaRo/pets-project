b85514aa1bc64666c5a25a14c26c2c397e0b11ea
maknetaRo <makneta@gmail.com>
2025-03-01 12:57:06 +0100

    start project

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+name = 'pies Pixel'
+
+@app.get('/')
+async def root():
+    return getPetName(name)
+
+def getPetName(name: str):
+    return name
+
